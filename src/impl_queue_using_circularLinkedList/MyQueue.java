db90033c9e692c0fed947f2f3832924f00613594
ChungNguyenVr912 <126665381+ChungNguyenVr912@users.noreply.github.com>
2023-04-14 17:11:00 +0700

    Create MyQueue.java

---
+++ src/impl_queue_using_circularLinkedList/MyQueue.java
@@ -0,0 +1,21 @@
+package impl_queue_using_circularLinkedList;
+
+public class MyQueue implements Solution{
+    Node front;
+    Node rear;
+
+    @Override
+    public void enQueue() {
+
+    }
+
+    @Override
+    public void deQueue() {
+
+    }
+
+    @Override
+    public void displayQueue() {
+
+    }
+}
