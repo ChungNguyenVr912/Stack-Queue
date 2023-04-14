3306c967438611efff0b238b9a2ebf60bcd4f9a7
ChungNguyenVr912 <126665381+ChungNguyenVr912@users.noreply.github.com>
2023-04-14 16:38:51 +0700

    Create Main.java

---
+++ src/char_count_using_Map/Main.java
@@ -0,0 +1,31 @@
+package char_count_using_Map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+        String string = "The Map interface provides three collection views";
+        Map<String, Integer> map = new TreeMap<>();
+        String[] array = string.split(" ");
+        string = "";
+        for (String s : array) {
+            string += s.toUpperCase();
+        }
+        System.out.println(string);
+        array = string.split("");
+        for (String s : array) {
+            if (map.containsKey(s)) {
+                map.put(s, map.get(s) + 1);
+            } else {
+                map.put(s, 1);
+            }
+        }
+        Set<Map.Entry<String,Integer>> set = map.entrySet();
+        for (Map.Entry<String, Integer> item : set){
+            System.out.println(item.getKey() + ": " + item.getValue());
+        }
+    }
+}
