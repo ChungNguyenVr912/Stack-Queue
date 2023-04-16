9ea4d5c157d0d53c0a454d2fa0327c21c3de0bb8
ChungNguyenVr912 <126665381+ChungNguyenVr912@users.noreply.github.com>
2023-04-16 16:47:25 +0700

    Update MyQueue.java

--- src/impl_queue_using_circularLinkedList/MyQueue.java
+++ src/impl_queue_using_circularLinkedList/MyQueue.java
@@ -5,17 +5,39 @@ public class MyQueue implements Solution{
     Node rear;
 
     @Override
-    public void enQueue() {
-
+    public void enQueue(int data) {
+        if (front == null){
+            front = rear = new Node(data);
+        }else {
+            rear.link = new Node(data);
+            rear = rear.link;
+            rear.link = front;
+        }
     }
 
     @Override
     public void deQueue() {
-
+        if (front == null){
+            System.out.println("Queue is empty!");
+        } else if (front == rear) {
+            front = rear = null;
+        }else {
+        front= front.link;
+        rear.link = front;
+        }
     }
 
     @Override
     public void displayQueue() {
-
+        if (front == null){
+            System.out.println("Queue is empty!");
+            return;
+        }
+        System.out.println(front.data);
+        Node temp = front.link;
+        while (temp != front){
+            System.out.println(temp.data);
+            temp = temp.link;
+        }
     }
 }
